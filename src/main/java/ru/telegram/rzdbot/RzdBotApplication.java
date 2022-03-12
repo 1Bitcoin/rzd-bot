51b0f400607e54b383815dd66258fdf34e5bd154
Дмитрий l W.W <shigalkin@mail.ru>
2022-03-12 19:47:00 +0300

    Initial commit

---
+++ src/main/java/ru/telegram/rzdbot/RzdBotApplication.java
@@ -0,0 +1,13 @@
+package ru.telegram.rzdbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RzdBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RzdBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/telegram/rzdbot/RzdBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.telegram.rzdbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RzdBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
